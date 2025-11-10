e1172ec1dc0549728eec715cf3d73da4f4ec15ec
lih7912 <lih7912@autostock.co.kr>
2025-11-10 15:17:06 +0900

    test

--- routes/nsrApi.js
+++ routes/nsrApi.js
@@ -26,161 +26,164 @@ nsrApi.all('/insert_docu_material/:user_id', async (req, res) => {
     
     let documentNo = `${today}-${String(maxInSq).padStart(5, '0')}-${String(maxLnSq).padStart(3, '0')}`;
 
-    await mssqlExec.mssqlExec(
-        `
-        INSERT INTO dbo.SAUTODOCUD (
-            IN_DT,        -- 처리일자 (PK)
-            IN_SQ,        -- 처리번호 (PK)
-            LN_SQ,        -- 처리라인번호 (PK)
-            CO_CD,        -- 회사코드 (PK)
-            IN_DIV_CD,    -- 처리사업장
-            LOGIC_CD,     -- 전표유형
-            ISU_DT,       -- 결의일자
-            ISU_SQ,       -- 결의번호
-            DIV_CD,       -- 회계단위
-            DEPT_CD,      -- 결의부서
-            EMP_CD,       -- 작업자
-            ACCT_CD,      -- 계정과목
-            DRCR_FG,      -- 차·대구분 (3:차변, 4:대변)
-            ACCT_AM,      -- 금액
-            RMK_NB,       -- 적요번호
-            RMK_DC,       -- 적요
-            ATTR_CD,      -- 증빙구분
-            TRCD_TY,      -- A_TY
-            TRNM_TY,      -- B_TY
-            DEPTCD_TY,    -- C_TY
-            PJTCD_TY,     -- D_TY
-            CTNB_TY,      -- E_TY
-            FRDT_TY,      -- F_TY
-            TODT_TY,      -- G_TY
-            QT_TY,        -- H_TY
-            AM_TY,        -- I_TY
-            RT_TY,        -- J_TY
-            DEAL_TY,      -- K_TY
-            USER1_TY,     -- L_TY
-            USER2_TY,     -- M_TY
-            TR_CD,        -- 거래처코드
-            TR_NM,        -- 거래처명
-            CT_DEPT,      -- C_TY 관련 코드
-            DEPT_NM,      -- C_TY 관련 코드명
-            PJT_CD,       -- D_TY 관련 코드
-            PJT_NM,       -- D_TY 관련 코드명
-            CT_NB,        -- E_TY 관련 코드
-            FR_DT,        -- 일자 (20061112)
-            TO_DT,        -- 일자 (20061112)
-            CT_QT,        -- 수량 (numeric)
-            CT_AM,        -- 금액 (numeric)
-            CT_RT,        -- 비율 (numeric)
-            CT_DEAL,      -- K_TY 코드
-            DEAL_NM,      -- K_TY 관련 코드명
-            CT_USER1,     -- L_TY 코드
-            USER1_NM,     -- CT_USER1 명
-            CT_USER2,     -- M_TY 코드
-            USER2_NM,     -- CT_USER2 명
-            EXCH_TY,      -- 외화종류
-            EXCH_AM,      -- 외화금액
-            PAYMENT,      -- 지급은행지점명
-            ISU_NM,       -- 발행인
-            ENDORS_NM,    -- 배서인
-            BILL_FG1,     -- 어음종류
-            BILL_FG2,     -- 수금구분
-            DUMMY1,       -- 여분1
-            DUMMY2,       -- 여분2
-            DUMMY3,       -- 여분3
-            INSERT_DT,    -- 입력일자
-            EX_FG,        -- 자료구분
-            TR_NMK,       -- 거래처명(다국어)
-            DEPT_NMK,     -- 부서명(다국어)
-            PJT_NMK,      -- 프로젝트명(다국어)
-            DEAL_NMK,     -- 거래유형명(다국어)
-            USER1_NMK,    -- 사용자1명(다국어)
-            USER2_NMK,    -- 사용자2명(다국어)
-            RMK_DCK,      -- 적요(다국어)
-            ISU_DOC,      -- 품의내역
-            ISU_DOCK,     -- 품의내역(다국어)
-            PRS_FG,       -- ?
-            JEONJA_YN,    -- 전자세금계산서여부
-            PAYMENT_PT,   -- 지급은행지점명
-            DEAL_FG       -- 보관구분
+    try {
+        await mssqlExec.mssqlExec(
+            `
+            INSERT INTO dbo.SAUTODOCUD (
+                IN_DT,        -- 처리일자 (PK)
+                IN_SQ,        -- 처리번호 (PK)
+                LN_SQ,        -- 처리라인번호 (PK)
+                CO_CD,        -- 회사코드 (PK)
+                IN_DIV_CD,    -- 처리사업장
+                LOGIC_CD,     -- 전표유형
+                ISU_DT,       -- 결의일자
+                ISU_SQ,       -- 결의번호
+                DIV_CD,       -- 회계단위
+                DEPT_CD,      -- 결의부서
+                EMP_CD,       -- 작업자
+                ACCT_CD,      -- 계정과목
+                DRCR_FG,      -- 차·대구분 (3:차변, 4:대변)
+                ACCT_AM,      -- 금액
+                RMK_NB,       -- 적요번호
+                RMK_DC,       -- 적요
+                ATTR_CD,      -- 증빙구분
+                TRCD_TY,      -- A_TY
+                TRNM_TY,      -- B_TY
+                DEPTCD_TY,    -- C_TY
+                PJTCD_TY,     -- D_TY
+                CTNB_TY,      -- E_TY
+                FRDT_TY,      -- F_TY
+                TODT_TY,      -- G_TY
+                QT_TY,        -- H_TY
+                AM_TY,        -- I_TY
+                RT_TY,        -- J_TY
+                DEAL_TY,      -- K_TY
+                USER1_TY,     -- L_TY
+                USER2_TY,     -- M_TY
+                TR_CD,        -- 거래처코드
+                TR_NM,        -- 거래처명
+                CT_DEPT,      -- C_TY 관련 코드
+                DEPT_NM,      -- C_TY 관련 코드명
+                PJT_CD,       -- D_TY 관련 코드
+                PJT_NM,       -- D_TY 관련 코드명
+                CT_NB,        -- E_TY 관련 코드
+                FR_DT,        -- 일자 (20061112)
+                TO_DT,        -- 일자 (20061112)
+                CT_QT,        -- 수량 (numeric)
+                CT_AM,        -- 금액 (numeric)
+                CT_RT,        -- 비율 (numeric)
+                CT_DEAL,      -- K_TY 코드
+                DEAL_NM,      -- K_TY 관련 코드명
+                CT_USER1,     -- L_TY 코드
+                USER1_NM,     -- CT_USER1 명
+                CT_USER2,     -- M_TY 코드
+                USER2_NM,     -- CT_USER2 명
+                EXCH_TY,      -- 외화종류
+                EXCH_AM,      -- 외화금액
+                PAYMENT,      -- 지급은행지점명
+                ISU_NM,       -- 발행인
+                ENDORS_NM,    -- 배서인
+                BILL_FG1,     -- 어음종류
+                BILL_FG2,     -- 수금구분
+                DUMMY1,       -- 여분1
+                DUMMY2,       -- 여분2
+                DUMMY3,       -- 여분3
+                INSERT_DT,    -- 입력일자
+                EX_FG,        -- 자료구분
+                TR_NMK,       -- 거래처명(다국어)
+                DEPT_NMK,     -- 부서명(다국어)
+                PJT_NMK,      -- 프로젝트명(다국어)
+                DEAL_NMK,     -- 거래유형명(다국어)
+                USER1_NMK,    -- 사용자1명(다국어)
+                USER2_NMK,    -- 사용자2명(다국어)
+                RMK_DCK,      -- 적요(다국어)
+                ISU_DOC,      -- 품의내역
+                ISU_DOCK,     -- 품의내역(다국어)
+                PRS_FG,       -- ?
+                JEONJA_YN,    -- 전자세금계산서여부
+                PAYMENT_PT,   -- 지급은행지점명
+                DEAL_FG       -- 보관구분
+            )
+            VALUES (
+                '${today}',      -- 처리일자
+                ${maxInSq},        -- 처리번호
+                ${maxLnSq},        -- 처리라인번호
+                '3000', -- 회사코드
+                '3000', -- 처리사업장
+                '21',   -- 전표유형 21-매입, 31-매출, 41-수금,51-반제
+                '00000000',     -- 결의일자
+                0,       -- 결의번호
+                NULL,     -- 회계단위
+                NULL,    -- 결의부서
+                NULL,     -- 작업자
+                '${input.cd_acct}',    -- 계정과목
+                '3',    -- 차대구분 3-차변, 4-대변
+                ${input.amt},      -- 금액
+                '0',     -- 적요번호
+                '${input.nm_note}',     -- 적요
+                NULL,    -- 증빙구분
+                NULL,    -- A_TY
+                NULL,    -- B_TY
+                NULL,  -- C_TY
+                NULL,   -- D_TY
+                NULL,    -- E_TY
+                NULL,    -- F_TY
+                NULL,    -- G_TY
+                NULL,      -- H_TY
+                NULL,      -- I_TY
+                NULL,      -- J_TY
+                NULL,    -- K_TY
+                NULL,   -- L_TY
+                NULL,   -- M_TY
+                NULL,      -- 거래처코드
+                NULL,      -- 거래처명
+                NULL,    -- C_TY 관련 코드
+                NULL,    -- C_TY 관련 코드명
+                NULL,     -- D_TY 관련 코드
+                NULL,     -- D_TY 관련 코드명
+                NULL,      -- E_TY 관련 코드
+                '00000000',      -- 시작일자
+                '00000000',      -- 종료일자
+                NULL,        -- 수량
+                NULL,        -- 금액
+                NULL,        -- 비율
+                NULL,    -- K_TY 코드
+                NULL,    -- K_TY 관련 코드명
+                NULL,   -- L_TY 코드
+                NULL,   -- CT_USER1 명
+                NULL,   -- M_TY 코드
+                NULL,   -- CT_USER2 명
+                NULL,    -- 외화종류
+                NULL,      -- 외화금액
+                NULL,    -- 지급은행지점명
+                NULL,     -- 발행인
+                NULL,  -- 배서인
+                NULL,   -- 어음종류
+                NULL,   -- 수금구분
+                NULL,     -- 여분1
+                NULL,     -- 여분2
+                NULL,     -- 여분3
+                '${today}',  -- 입력일자
+                '0',      -- 자료구분
+                NULL,     -- 거래처명(다국어)
+                NULL,   -- 부서명(다국어)
+                NULL,    -- 프로젝트명(다국어)
+                NULL,   -- 거래유형명(다국어)
+                NULL,  -- 사용자1명(다국어)
+                NULL,  -- 사용자2명(다국어)
+                NULL,    -- 적요(다국어)
+                NULL,    -- 품의내역
+                NULL,   -- 품의내역(다국어)
+                NULL,     -- ?
+                NULL,  -- 전자세금계산서여부
+                NULL, -- 지급은행지점명
+                NULL     -- 보관구분
+            );
+            `
         )
-        VALUES (
-            '${today}',      -- 처리일자
-            ${maxInSq},        -- 처리번호
-            ${maxLnSq},        -- 처리라인번호
-            '3000', -- 회사코드
-            '3000', -- 처리사업장
-            '21',   -- 전표유형 21-매입, 31-매출, 41-수금,51-반제
-            '00000000',     -- 결의일자
-            0,       -- 결의번호
-            NULL,     -- 회계단위
-            NULL,    -- 결의부서
-            NULL,     -- 작업자
-            '${input.strAcct}',    -- 계정과목
-            '3',    -- 차대구분 3-차변, 4-대변
-            ${input.amt},      -- 금액
-            '0',     -- 적요번호
-            '${input.nm_note}',     -- 적요
-            NULL,    -- 증빙구분
-            NULL,    -- A_TY
-            NULL,    -- B_TY
-            NULL,  -- C_TY
-            NULL,   -- D_TY
-            NULL,    -- E_TY
-            NULL,    -- F_TY
-            NULL,    -- G_TY
-            NULL,      -- H_TY
-            NULL,      -- I_TY
-            NULL,      -- J_TY
-            NULL,    -- K_TY
-            NULL,   -- L_TY
-            NULL,   -- M_TY
-            NULL,      -- 거래처코드
-            NULL,      -- 거래처명
-            NULL,    -- C_TY 관련 코드
-            NULL,    -- C_TY 관련 코드명
-            NULL,     -- D_TY 관련 코드
-            NULL,     -- D_TY 관련 코드명
-            NULL,      -- E_TY 관련 코드
-            '00000000',      -- 시작일자
-            '00000000',      -- 종료일자
-            NULL,        -- 수량
-            NULL,        -- 금액
-            NULL,        -- 비율
-            NULL,    -- K_TY 코드
-            NULL,    -- K_TY 관련 코드명
-            NULL,   -- L_TY 코드
-            NULL,   -- CT_USER1 명
-            NULL,   -- M_TY 코드
-            NULL,   -- CT_USER2 명
-            NULL,    -- 외화종류
-            NULL,      -- 외화금액
-            NULL,    -- 지급은행지점명
-            NULL,     -- 발행인
-            NULL,  -- 배서인
-            NULL,   -- 어음종류
-            NULL,   -- 수금구분
-            NULL,     -- 여분1
-            NULL,     -- 여분2
-            NULL,     -- 여분3
-            '${today}',  -- 입력일자
-            '0',      -- 자료구분
-            NULL,     -- 거래처명(다국어)
-            NULL,   -- 부서명(다국어)
-            NULL,    -- 프로젝트명(다국어)
-            NULL,   -- 거래유형명(다국어)
-            NULL,  -- 사용자1명(다국어)
-            NULL,  -- 사용자2명(다국어)
-            NULL,    -- 적요(다국어)
-            NULL,    -- 품의내역
-            NULL,   -- 품의내역(다국어)
-            NULL,     -- ?
-            NULL,  -- 전자세금계산서여부
-            NULL, -- 지급은행지점명
-            NULL     -- 보관구분
-        );
-        `
-    )
-    
+    } catch (e) {
+        console.log(e);
+    }
 
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end(JSON.stringify([ {INVOICE_NO: input.nm_note, DOCU_NO: documentNo} ]));
