d8bb0bf1986bd49c76f550143f526f9f5919ede6
lih7912 <lih7912@autostock.co.kr>
2025-11-11 15:14:13 +0900

    test

--- routes/nsrApi.js
+++ routes/nsrApi.js
@@ -4,17 +4,56 @@ const moment = require('moment');
 const nsrApi = router();
 
 nsrApi.all('/insert_docu_material/:user_id', async (req, res) => {
-    let input = [...req.body][0].DATA1
-    let today = moment().format('YYYYMMDD');
-
-    console.log('-------------------', input);
-
+    let input = [...req.body][0].DATA1;
+    const today = moment().format('YYYYMMDD');
+    
     if (!input) {
         res.end(JSON.stringify([ {INVOICE_NO: -1, DOCU_NO: -1} ]));   
     }
-    
+
+    /*** 계정과목 NSR에 맞게 변환 ***/
+    input.ct_deal = '';
+    // 부가세대급금
+    if (input.cd_acct === '15400') {
+        input.cd_acct = '13500';
+        
+        // 부가세대급금(면세)
+        if (input.tp_tax === '26') {
+            input.tp_tax = '23';
+            input.ct_deal = '면세'
+        }
+
+        // 부가세대급금(영세)
+        if (input.tp_tax === '23') {
+            input.tp_tax = '22';
+            input.ct_deal = '영세'
+        }
+
+        // 부가세대급금(과세)
+        if (input.tp_tax === '21') {
+            input.tp_tax = '21';
+            input.ct_deal = '과세'
+        }
+    }
+
+    // 원재료
+    if (input.cd_acct === '15400') {
+        input.cd_acct = '14900';
+    }
+
+    // 미착품
+    if (input.cd_acct === '16500') {
+        input.cd_acct = '15600';
+    }
+
+    // 상품
+    if (input.cd_acct === '15100') {
+        input.cd_acct = '14600';
+    }
+    /*** 계정과목 NSR에 맞게 변환 ***/
+        
     let maxInSq = await mssqlExec.mssqlExec(
-        `SELECT ISNULL(MAX(IN_SQ), 0) AS MAX_IN_SQ FROM dbo.SAUTODOCUD WHERE IN_DT = '${today}'`
+        `SELECT ISNULL(MAX(IN_SQ), 0) AS MAX_IN_SQ FROM dbo.SAUTODOCUD`
     );
 
     maxInSq = maxInSq[0].MAX_IN_SQ + 1;
@@ -23,8 +62,57 @@ nsrApi.all('/insert_docu_material/:user_id', async (req, res) => {
         `SELECT ISNULL(MAX(LN_SQ), 0) AS MAX_LN_SQ FROM dbo.SAUTODOCUD WHERE IN_DT = '${today}' AND IN_SQ = ${maxInSq}`
     );
     maxLnSq = maxLnSq[0].MAX_LN_SQ + 1;
+
+    let maxIsuSq = await mssqlExec.mssqlExec(
+        `SELECT ISNULL(MAX(ISU_SQ), 0) AS MAX_ISU_SQ FROM dbo.SAUTODOCUD WHERE IN_DT = '${today}'`
+    );
+    maxIsuSq = maxIsuSq[0].MAX_ISU_SQ + 1;
     
     let documentNo = `${today}-${String(maxInSq).padStart(5, '0')}-${String(maxLnSq).padStart(3, '0')}`;
+
+    await execDbInsert('차변', input, maxInSq, maxLnSq, maxIsuSq, res);
+    await execDbInsert('대변', input, maxInSq, maxLnSq++, maxIsuSq, res);
+    await execDbInsert('부가세', input, maxInSq, maxLnSq++, maxIsuSq, res);
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(JSON.stringify([ {INVOICE_NO: input.nm_note, DOCU_NO: documentNo} ]));
+});
+
+async function execDbInsert(mode, input, maxInSq, maxLnSq, maxIsuSq, res) {
+    const 차변   = mode === '차변';
+    const 대변   = mode === '대변';
+    const 부가세 = mode === '부가세';
+
+    const today = moment().format('YYYYMMDD');
+
+    let vendorName = input.nm_mngd1.replace(/\(.*?\)/g, '').trim();
+    let vendorInfo = await mssqlExec.mssqlExec(
+        `
+        SELECT * FROM ZA_TRADE_DAIKIN WHERE TR_NM LIKE '%${vendorName}%'
+        `
+    );
+    if (!vendorInfo?.length) {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(JSON.stringify([ {error: '거래처 정보를 찾을 수 없습니다.'} ]));
+        throw new Error('거래처 정보를 찾을 수 없습니다.');
+    }
+
+    vendorName = vendorInfo[0].TR_NM;
+    let vendorCd = vendorInfo[0].TR_CD;
+
+    let AMT = input.amt;  // 부가세 포함금액 (총액)
+    const vatRate = 0.1;  // 부가세율 10%
+
+    const supplyAmt = Math.round(AMT / (1 + vatRate));  // 원금
+    const vatAmt = AMT - supplyAmt;                     // 부가세
+
+    if (차변) {
+        AMT = supplyAmt;
+    } else if (대변) {
+        AMT = input.amt;
+    } else if (부가세) {
+        AMT = vatAmt;
+    }
 
     try {
         await mssqlExec.mssqlExec(
@@ -42,7 +130,7 @@ nsrApi.all('/insert_docu_material/:user_id', async (req, res) => {
                 DEPT_CD,      -- 결의부서
                 EMP_CD,       -- 작업자
                 ACCT_CD,      -- 계정과목
-                DRCR_FG,      -- 차·대구분 (3:차변, 4:대변)
+                DRCR_FG,      -- 차·대구분 (차변 3, 대변 4, 부가세 3)
                 ACCT_AM,      -- 금액
                 RMK_NB,       -- 적요번호
                 RMK_DC,       -- 적요
@@ -105,88 +193,88 @@ nsrApi.all('/insert_docu_material/:user_id', async (req, res) => {
                 DEAL_FG       -- 보관구분
             )
             VALUES (
-                '${today}',      -- 처리일자
-                ${maxInSq},        -- 처리번호
-                ${maxLnSq},        -- 처리라인번호
-                '3000', -- 회사코드
-                '3000', -- 처리사업장
-                '21',   -- 전표유형 21-매입, 31-매출, 41-수금,51-반제
-                '00000000',     -- 결의일자
-                0,       -- 결의번호
-                NULL,     -- 회계단위
-                NULL,    -- 결의부서
-                NULL,     -- 작업자
-                '${input.cd_acct}',    -- 계정과목
-                '3',    -- 차대구분 3-차변, 4-대변
-                ${input.amt},      -- 금액
-                '0',     -- 적요번호
-                '${input.nm_note}',     -- 적요
-                NULL,    -- 증빙구분
-                NULL,    -- A_TY
-                NULL,    -- B_TY
-                NULL,  -- C_TY
-                NULL,   -- D_TY
-                NULL,    -- E_TY
-                NULL,    -- F_TY
-                NULL,    -- G_TY
-                NULL,      -- H_TY
-                NULL,      -- I_TY
-                NULL,      -- J_TY
-                NULL,    -- K_TY
-                NULL,   -- L_TY
-                NULL,   -- M_TY
-                NULL,      -- 거래처코드
-                NULL,      -- 거래처명
-                NULL,    -- C_TY 관련 코드
-                NULL,    -- C_TY 관련 코드명
-                NULL,     -- D_TY 관련 코드
-                NULL,     -- D_TY 관련 코드명
-                NULL,      -- E_TY 관련 코드
-                '00000000',      -- 시작일자
-                '00000000',      -- 종료일자
-                NULL,        -- 수량
-                NULL,        -- 금액
-                NULL,        -- 비율
-                NULL,    -- K_TY 코드
-                NULL,    -- K_TY 관련 코드명
-                NULL,   -- L_TY 코드
-                NULL,   -- CT_USER1 명
-                NULL,   -- M_TY 코드
-                NULL,   -- CT_USER2 명
-                NULL,    -- 외화종류
-                NULL,      -- 외화금액
-                NULL,    -- 지급은행지점명
-                NULL,     -- 발행인
-                NULL,  -- 배서인
-                NULL,   -- 어음종류
-                NULL,   -- 수금구분
-                NULL,     -- 여분1
-                NULL,     -- 여분2
-                NULL,     -- 여분3
-                '${today}',  -- 입력일자
-                '0',      -- 자료구분
-                NULL,     -- 거래처명(다국어)
-                NULL,   -- 부서명(다국어)
-                NULL,    -- 프로젝트명(다국어)
-                NULL,   -- 거래유형명(다국어)
-                NULL,  -- 사용자1명(다국어)
-                NULL,  -- 사용자2명(다국어)
-                NULL,    -- 적요(다국어)
-                NULL,    -- 품의내역
-                NULL,   -- 품의내역(다국어)
-                NULL,     -- ?
-                NULL,  -- 전자세금계산서여부
-                NULL, -- 지급은행지점명
-                NULL     -- 보관구분
+                '${today}',           -- 처리일자
+                ${maxInSq},           -- 처리번호
+                ${maxLnSq},           -- 처리라인번호
+                '1000',               -- 회사코드
+                '1000',               -- 처리사업장
+                '21',                 -- 전표유형 (21: 매입, 31: 매출, 41: 수금, 51: 반제)
+                '${today}',           -- 결의일자
+                ${maxIsuSq},          -- 결의번호
+                '1000',               -- 회계단위
+                '0800',               -- 결의부서
+                'N250519',            -- 작업자
+                '${input.cd_acct}',   -- 계정과목
+                ${차변 ? '3'  : 대변 ? '4' : '3'},         -- 차대구분 (차변 3, 대변 4, 부가세 3)
+                ${AMT},               -- 금액
+                '',                   -- 적요번호
+                '${input.nm_note}',   -- 적요
+                'A1',                 -- 증빙구분
+                'B1',                 -- A_TY
+                'C1',                 -- B_TY
+                ${차변 ? '0'  : 대변 ? 'D5' : '0'},        -- C_TY (차변 0, 대변 D5, 부가세 0)
+                ${차변 ? 'E6' : '0'},                      -- D_TY 사업장 (차변 E6, 대변 0, 부가세 0)
+                ${대변 ? 'F3' : 부가세 ? 'F1' : '0'},      -- E_TY 관리번호 (차변 0, 대변 F3, 부가세 F1)
+                0,                    -- F_TY 발생일자
+                ${차변 ? 'H1' : 부가세 ? 'H3' : '0'},      -- G_TY (차변 H1, 대변 0, 부가세 H3)
+                ${차변 ? 'I4' : 부가세 ? 'I3' : '0'},      -- H_TY (차변 I4, 대변 0, 부가세 I3)
+                0,                    -- I_TY
+                ${차변 ? 'K6' : 부가세 ? `'K1'` : '0'},    -- J_TY (차변 K6, 대변 0, 부가세 K1)
+                0,                    -- K_TY 
+                0,                    -- L_TY
+                NULL,                 -- M_TY
+                '${vendorCd}',        -- 거래처코드
+                '${vendorName}',      -- 거래처명
+                ${차변 ? '1000' : 대변 ? '1000' : ''},     -- C_TY 관련 코드 (차변 1000, 대변 1000, 부가세 '')
+                NULL,                 -- C_TY 관련 코드명
+                ${부가세 ? '1000' : ''},                   -- D_TY 관련 코드 (차변 '', 대변 '', 부가세 1000)
+                ${부가세 ? '(주)엔에스알' : ''},            -- D_TY 관련 코드명 (부가세 (주)엔에스알)
+                0,                    -- E_TY 관련 코드 
+                '00000000',           -- 시작일자
+                '${today}',           -- 종료일자 (부가세 today)
+                0,                    -- 수량
+                0,                    -- 금액 (부가세일 경우 부가세 금액)
+                0,                    -- 비율
+                ${input.tp_tax ?? 0}, -- K_TY 코드
+                ${input.ct_deal ?? 0},-- K_TY 관련 코드명
+                ${차변 ? '일반' : 대변 ? 'NULL' : '과세매입'},  -- L_TY 코드 (차변 일반, 대변 null, 부가세 과세매입)
+                0,                    -- CT_USER1 명
+                0,                    -- M_TY 코드
+                0,                    -- CT_USER2 명
+                NULL,                 -- 외화종류
+                NULL,                 -- 외화금액
+                NULL,                 -- 지급은행지점명
+                NULL,                 -- 발행인
+                NULL,                 -- 배서인
+                NULL,                 -- 어음종류
+                NULL,                 -- 수금구분
+                NULL,                 -- 여분1
+                NULL,                 -- 여분2
+                NULL,                 -- 여분3
+                GETDATE(),            -- 입력일자
+                '1',                  -- 자료구분
+                NULL,                 -- 거래처명(다국어)
+                NULL,                 -- 부서명(다국어)
+                NULL,                 -- 프로젝트명(다국어)
+                NULL,                 -- 거래유형명(다국어)
+                NULL,                 -- 사용자1명(다국어)
+                NULL,                 -- 사용자2명(다국어)
+                '원부자재',            -- 적요(다국어)
+                '',                   -- 품의내역
+                NULL,                 -- 품의내역(다국어)
+                NULL,                 -- 미정 필드 (?)
+                NULL,                 -- 전자세금계산서 여부
+                NULL,                 -- 지급은행지점명
+                NULL                  -- 보관구분
             );
             `
         )
     } catch (e) {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(JSON.stringify([ {error: e.message} ]));
         console.log(e);
+        return;
     }
-
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(JSON.stringify([ {INVOICE_NO: input.nm_note, DOCU_NO: documentNo} ]));
-});
+}
 
 module.exports = nsrApi;
